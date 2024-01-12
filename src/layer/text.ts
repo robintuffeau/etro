a3d3f2bb9038636da888d42c13d7399a9c71f475
Robin <tuffeau.robin@gmail.com>
2024-01-12 19:28:09 +0100

    up

--- src/layer/text.ts
+++ src/layer/text.ts
@@ -126,7 +126,7 @@ class Text extends Visual {
       const textX = val(this, 'textX', this.currentTime);
       const textY = val(this, 'textY', this.currentTime);
       const rectX = textX - rectWidth / 2;
-      const rectY = textY - (((fontSize * 1.2) - fontSize) / 2) - padding / 2;
+      const rectY = textY - ((fontSize * 1.2) - fontSize) - padding / 2;
 
       this.cctx.fillStyle = textBackground;
       this.cctx.beginPath();
